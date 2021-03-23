0c32b308d59326c94952a08461e42212b51768d0
Vasek, Lukas <lvasek@gk-software.com>
2021-03-23 19:30:28 +0100

    Issue with repository not using querydsl

--- src/main/java/com/github/bilakpoc/springjdbcissues/data/repository/UserRepository.java
+++ src/main/java/com/github/bilakpoc/springjdbcissues/data/repository/UserRepository.java
@@ -1,11 +1,22 @@
 package com.github.bilakpoc.springjdbcissues.data.repository;
 
+import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-
+import com.github.bilakpoc.springjdbcissues.data.entity.QUserEntity;
 import com.github.bilakpoc.springjdbcissues.data.entity.UserEntity;
+import com.infobip.spring.data.jdbc.QuerydslJdbcRepository;
 
-public interface UserRepository extends PagingAndSortingRepository<UserEntity, UUID> {
-
+public interface UserRepository extends QuerydslJdbcRepository<UserEntity, UUID> {
+  
+  QUserEntity USER = QUserEntity.userEntity;
+  
+  default List<UserEntity> findByName(String name) {
+    final String fulltext = "%".concat(name).concat("%");
+    return queryMany(q -> q
+      .select(entityProjection())
+      .from(USER)
+      .where(USER.firstName.lower().like(fulltext).or(USER.lastName.lower().like(fulltext))));
+  }
+  
 }
--- src/test/java/com/github/bilakpoc/springjdbcissues/data/repository/UserRepositoryTest.java
+++ src/test/java/com/github/bilakpoc/springjdbcissues/data/repository/UserRepositoryTest.java
@@ -9,9 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.github.bilakpoc.springjdbcissues.AbstractIT;
+import com.github.bilakpoc.springjdbcissues.data.entity.QUserEntity;
 import com.github.bilakpoc.springjdbcissues.data.entity.UserEntity;
 
 class UserRepositoryTest extends AbstractIT {
+  
+  private final QUserEntity USER = QUserEntity.userEntity;
   
   @Autowired
   private UserRepository userRepository;
@@ -20,6 +23,14 @@ class UserRepositoryTest extends AbstractIT {
   void testFindUser() {
     final Optional<UserEntity> userOpt = userRepository.findById(ALISA_PEKERS);
     
+    assertThat(userOpt.isPresent(), is(true));
+  }
+  
+  @Test
+  void testQueryDsl() {
+    final Optional<UserEntity> userOpt = userRepository.queryOne(
+      q -> q.select(userRepository.entityProjection()).where(USER.firstName.eq("John")));
+  
     assertThat(userOpt.isPresent(), is(true));
   }
   
